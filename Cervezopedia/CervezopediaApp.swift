584eba969572fada21ba327963dffdf88bfa7ed1
Gorka Puente Díez <gorkapuente@usal.es>
2023-12-13 19:52:31 +0100

    Initial Commit

---
+++ Cervezopedia/CervezopediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CervezopediaApp.swift
+//  Cervezopedia
+//
+//  Created by Gorka Puente Díez on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CervezopediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
